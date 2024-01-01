5d870fe08d5d8291c1ae38e56d8068eb33015c47
AbramKanime <111146026+AbramKanime@users.noreply.github.com>
2024-01-01 15:57:36 +0100

    updated app and css

--- App.jsx
+++ App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Sidebar'
 import Dashboard from './components/Dashboard'
 
